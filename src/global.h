542a41986b403e11e22c3b0fb2a7f6fd916ad40a
Jura Prokofev <Smyrke2005@yandex.ru>
2020-02-17 17:46:06 +0300

    Update drawLogs.pro, main.cpp, and 28 more files...

---
+++ src/global.h
@@ -0,0 +1,20 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <QString>
+#include <QDir>
+
+struct Config{
+	bool showData			= true;
+	bool verbose			= true;
+	uint8_t logLevel		= 3;
+	QString logFile			= "/tmp/mystats.log";
+};
+
+namespace app {
+	extern Config conf;
+
+	void setLog(const uint8_t logLevel, const QString &mess);
+}
+
+#endif // GLOBAL_H

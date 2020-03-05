9b0136e0e4541e13f0c011d15a26d811c2356665
Jura Prokofiev <Smyrke2005@yandex.ru>
2020-03-05 12:36:58 +0300

    Update autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -1,3 +1,12 @@
+if [ $1 = "getVersion" ]; then
+	if [ -f "src/version.h" ]; then
+		cat "src/version.h" | awk '{print $3}'
+	else
+		echo "N/A"
+	fi
+	exit 0
+fi
+
 cd src
 qmake version_desktop.pro
 make

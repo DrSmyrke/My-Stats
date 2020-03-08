898d31ec2d13a00dd11b8314cd439eee98e08fa7
DrSmyrke <Smyrke2005@yandex.ru>
2020-03-08 12:09:13 +0300

    Update autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -1,11 +1,14 @@
-if [ $1 = "--version" ]; then
-	if [ -f "src/version.h" ]; then
-		cat "src/version.h" | awk '{print $3}'
-	else
-		echo "N/A"
+if [ $1 ]; then
+	if [ $1 = "--version" ]; then
+		if [ -f "src/version.h" ]; then
+			cat "src/version.h" | awk '{print $3}'
+		else
+			echo "N/A"
+		fi
+		exit 0
 	fi
-	exit 0
 fi
+
 
 cd src
 qmake version_desktop.pro
